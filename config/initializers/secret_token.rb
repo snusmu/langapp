c68be3cb82332cf6bb27ca058b7c2b817a717a84
SNVSMV <snvsmv@snvsmv-wk.(none)>
2013-09-09 00:38:05 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Langapp::Application.config.secret_key_base = '208bcbfadb07ff9bd2c7b82a72d349c26ac12f595356a6ed96b4ee0eaf6502b4b35ecfe9d6a5ab8348670b349b8e09096af98667606c55f8b3ff534801050668'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Langapp::Application.config.session_store :cookie_store, key: '_langapp_session'
